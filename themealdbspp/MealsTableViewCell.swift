6272a1ab0895d0659fe892dafc1791b783cce72a
Kwaku Dapaah <kwakudapaah@Kwakus-Mac-mini.local>
2023-06-19 15:28:24 -0400

    created skeleton for themealdb project, created table view

---
+++ themealdbspp/MealsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealsTableViewCell.swift
+//  themealdbspp
+//
+//  Created by Kwaku Dapaah on 6/19/23.
+//
+
+import UIKit
+
+class MealsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ themealdbspp/MealsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MealsViewController.swift
+//  themealdbspp
+//
+//  Created by Kwaku Dapaah on 6/19/23.
+//
+
+import UIKit
+
+class MealsViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableview = UITableView()
+        tableview.translatesAutoresizingMaskIntoConstraints = false
+        return tableview
+    }()
+    
+    private let meals = ["Jollof", "Burger", "Pie", "Biriyani"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            
+        ])
+    }
+    
+
+    
+
+}
+
+extension MealsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .systemMint
+        cell.textLabel?.text = "Food"
+        return cell
+    }
+    
+    
+}
